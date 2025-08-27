36f26259817579eb346b7764100428e2cf035f85
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-08-27 12:43:07 +0600

    added the calling function

--- script.js
+++ script.js
@@ -32,25 +32,58 @@ for (const hart of allHart) {
 }
 
 //call function
+const callData = []; 
 
 const callFunction = document.getElementsByClassName('call')
 for (const calls of callFunction) {
   calls.addEventListener('click', function () {
     const parentDiv = calls.closest('div')
-    const serviceName = parentDiv.querySelector('.service-name').innerText;
-    const serviceNumber = parentDiv.querySelector('.service-number').innerText;
+    const serviceName = parentDiv.querySelector('.services-name').innerText;
+     const callServicesName = parentDiv.querySelector(".callServices").innerText;
+    const serviceNum = parentDiv.querySelector('.services-num').innerText;
     const coinValue = strToNum('coins');
     if (coinValue < 20) {
       alert('তোমার পর্যাপ্ত পরিমাণে কয়েন নাই । তোমাকে কল করার জন্য কমপক্ষে ২০ টা কয়েন থাকতে হবে।');
       return;
     }
-    alert(`calling ${serviceName} ${serviceNumber}...`);
+    alert(`calling ${serviceName} ${serviceNum}...`);
     const totalCoin = coinValue - 20;
     document.getElementById('coins').innerText = totalCoin;
-    }) 
+    
+    const data = {
+      name: callServicesName,
+      num: serviceNum,
+      date: new Date().toLocaleTimeString(),
+    };
+
+    callData.push(data);
+    const parent = document.getElementById('history-container');
+    parent.innerText = "";
+
+    for (const value of callData) {
+      const div = document.createElement('div');
+      div.innerHTML = `
+         <div id='div' class="flex justify-between items-center px-3 mb-3">
+            
+            <div class="">
+              <h2 class="font-bold text-[14px]">${value.name}</h2>
+              <p class="font-semibold text-xs text-gray-500">${value.num}</p>
+            </div>
+            <p class="text-xs">${value.date}</p>
+          </div>
+    `;
+      parent.appendChild(div);
+    }
+  });
+}
+  
+document.getElementById('clear').addEventListener("click",function(){
+  const parent = document.getElementById("history-container");
+  parent.innerText = ''; 
+  callData.length=0;
+})
   
 
 
-   }
-   
-
+  
+  
